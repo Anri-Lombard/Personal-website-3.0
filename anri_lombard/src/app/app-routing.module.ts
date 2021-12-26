260a55415f20d8e1b9289649d5a035feab9a7004
Anri Lombard <anri.m.lombard@gmail.com>
2021-12-26 06:07:17 +0200

    Progress with 3.0

--- anri_lombard/src/app/app-routing.module.ts
+++ anri_lombard/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
